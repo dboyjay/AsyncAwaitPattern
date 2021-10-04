654b36f1a551c0d0755bc990389fa0d94285bd7b
heedolle <heedolle@gmail.com>
2021-10-05 01:23:07 +0900

    Fetching data with Async Await in Swift 5.5

--- AsyncAwaitPattern/ContentView.swift
+++ AsyncAwaitPattern/ContentView.swift
@@ -1,16 +1,99 @@
-//
-//  ContentView.swift
-//  AsyncAwaitPattern
-//
-//  Created by HMK on 2021/10/04.
-//
-
 import SwiftUI
 
+struct Course: Decodable, Identifiable {
+    
+    let id, numberOfLessons: Int
+    let name, link, imageUrl: String
+    
+}
+
+class ContentViewModel: ObservableObject {
+    
+    @Published var isFetching = false
+    @Published var courses = [Course]()
+    @Published var errorMessage = ""
+    
+    init() {
+        //Fetch data
+//
+    }
+    
+    @MainActor
+    func fetchData() async {
+        let urlString = "https://api.letsbuildthatapp.com/jsondecodable/courses"
+        
+        guard let url = URL(string: urlString) else {
+            print("error: can not fetch data")
+            return
+        }
+        
+        do {
+            isFetching = true
+            let (data, response) = try await URLSession.shared.data(from: url, delegate: nil)
+            
+            if let resp = response as? HTTPURLResponse, resp.statusCode >= 300 {
+                self.errorMessage = "failed to hit endpoint with bad status code"
+            }
+            //            print(data)
+            
+            self.courses = try JSONDecoder().decode([Course].self, from: data)
+            isFetching = false
+            //            print(courses)
+        } catch {
+            isFetching = false
+            print("Failed to reac endpoint: \(error)")
+        }
+        
+        
+    }
+}
+
 struct ContentView: View {
+    
+    @ObservedObject var vm = ContentViewModel()
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView {
+            ScrollView {
+                if vm.isFetching {
+                    ProgressView()
+                }
+                
+                VStack {
+                    ForEach(vm.courses) { course in
+                        let url = URL(string: course.imageUrl)
+                        AsyncImage(url: url) { image in
+                            image.resizable()
+                                .scaledToFit()
+                        } placeholder: {
+                            ProgressView()
+                        }
+                        
+                        Text(course.name)
+                        
+                    }
+                }
+                
+                
+                //                Text("Here is my scrollView")
+            }
+            .navigationTitle("Courses")
+            .task {
+                await vm.fetchData()
+            }
+            .navigationBarItems(trailing: refreshButton)
+        }
+    }
+    private var refreshButton: some View {
+        Button {
+            Task.init {
+                vm.courses.removeAll()
+                await  vm.fetchData()
+            }
+        } label: {
+            Text("Refresh")
+        }
+        
     }
 }
 
