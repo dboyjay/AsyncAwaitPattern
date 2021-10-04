993a6ac0f981bcb94a7ce3b7363ce13871a19924
heedolle <heedolle@gmail.com>
2021-10-04 23:14:26 +0900

    Initial Commit

---
+++ AsyncAwaitPattern/AsyncAwaitPatternApp.swift
@@ -0,0 +1,17 @@
+//
+//  AsyncAwaitPatternApp.swift
+//  AsyncAwaitPattern
+//
+//  Created by HMK on 2021/10/04.
+//
+
+import SwiftUI
+
+@main
+struct AsyncAwaitPatternApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AsyncAwaitPattern/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  AsyncAwaitPattern
+//
+//  Created by HMK on 2021/10/04.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
